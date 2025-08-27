f0edc1a9f7ee964d5c047a675ad6dd59729bd4a2
itilrozz <ethelrosebaricuatro75@gmail.com>
2025-08-27 09:46:33 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAdZN2lJJDbiQfM09cwEmpN4VlowFmQTAg",
+  authDomain: "erb-goals.firebaseapp.com",
+  projectId: "erb-goals",
+  storageBucket: "erb-goals.firebasestorage.app",
+  messagingSenderId: "651626462935",
+  appId: "1:651626462935:web:3d25b226d8a66eadebba92"
+};
+
+
+const app = initializeApp(firebaseConfig);
