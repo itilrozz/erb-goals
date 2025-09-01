0bb1c099ab75679c93ee5946bcb9865f69182037
itilrozz <ethelrosebaricuatro75@gmail.com>
2025-09-01 22:02:57 +0800

    console log

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,5 +1,6 @@
 
 import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAdZN2lJJDbiQfM09cwEmpN4VlowFmQTAg",
@@ -12,3 +13,4 @@ const firebaseConfig = {
 
 
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
